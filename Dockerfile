409fa468ff42c7330cbd12bf1e5674c0af994f7b
Andre Maciel Fagundes <amfcom@gmail.com>
2024-02-02 13:13:33 -0300

    fix: :bug: Docker Environment correction

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,22 @@
 FROM node:latest
 
+ENV DOCKERIZE_VERSION v0.7.0
+
+RUN apt-get update \
+    && apt-get install -y wget \
+    && wget -O - https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz | tar xzf - -C /usr/local/bin \
+    && apt-get autoremove -yqq --purge wget && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /app
 
 COPY package*.json ./
-
-RUN npm install
 
 COPY . .
 
 EXPOSE 3000
 
-CMD ["npx", "nodemon", "./src/index.ts"]
+CMD dockerize -wait tcp://db:3306 -timeout 180s \
+    && npm install \
+    && npm run prisma:dev \
+    && npx prisma db seed \
+    && npm run start
--- prisma/seed/user-seed.ts
+++ prisma/seed/user-seed.ts
@@ -1,43 +1,55 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 import bcrypt from 'bcrypt'
 
 const prisma = new PrismaClient()
 
-async function main() {
+async function seedData() {
   const hashedPassword = await bcrypt.hash('senha123', 10)
 
-  await prisma.user.createMany({
-    data: [
-      {
-        email: 'andremacielfagundes@example.com',
-        fullName: 'André Maciel Fagundes',
-        document: '92945874587',
-        wallet: 1000,
-        password: hashedPassword,
+  const usersToCreate: Omit<User, 'id' | 'createdAt' | 'updatedAt'>[] = [
+    {
+      email: 'andremacielfagundes@example.com',
+      fullName: 'André Maciel Fagundes',
+      document: '92945874587',
+      wallet: 1000,
+      password: hashedPassword,
+      isMerchant: false,
+    },
+    {
+      email: 'adriano@example.com',
+      fullName: 'Adriano Henrique',
+      document: '65478458748',
+      wallet: 10000,
+      password: hashedPassword,
+      isMerchant: false,
+    },
+    {
+      email: 'empresa@example.com',
+      fullName: 'Empresa D',
+      document: '56458795000132',
+      wallet: 50,
+      password: hashedPassword,
+      isMerchant: true,
+    },
+  ]
+
+  for (const user of usersToCreate) {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        document: user.document,
       },
-      {
-        email: 'adriano@example.com',
-        fullName: 'Adriano Henrique',
-        document: '65478458748',
-        wallet: 10000,
-        password: hashedPassword,
-      },
-      {
-        email: 'empresa@example.com',
-        fullName: 'Empresa D',
-        document: '56458795000132',
-        wallet: 50,
-        password: hashedPassword,
-        isMerchant: true
-      },
-    ],
-  })
+    })
+
+    if (!existingUser) {
+      await prisma.user.create({
+        data: user as User,
+      })
+    }
+  }
+
+  await prisma.$disconnect()
 }
 
-main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+seedData().catch((err) => {
+  throw err
+})
