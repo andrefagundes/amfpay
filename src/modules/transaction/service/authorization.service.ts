bb97ab9855cb1b83210694ff4bc898277b498d8f
Andre Maciel Fagundes <amfcom@gmail.com>
2024-02-05 11:54:08 -0300

    refactor: :wrench: changed config

--- src/modules/transaction/service/authorization.service.ts
+++ src/modules/transaction/service/authorization.service.ts
@@ -1,10 +1,9 @@
-import { TransactionConstants } from '../@shared/transaction.constants'
 import { AuthorizationServiceInterface } from './authorization.interface'
 
 export class AuthorizationService implements AuthorizationServiceInterface {
   async checkAuthorization(): Promise<boolean> {
     try {
-      const response = await fetch(TransactionConstants.AUTH_SERVICE_URL)
+      const response = await fetch(process.env.AUTH_SERVICE_URL)
       const data = await response.json()
 
       if (!data.approve) {
