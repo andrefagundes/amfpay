3249bd890b324d14d9de18d96ec1382d11eba0dc
Andre Maciel Fagundes <amfcom@gmail.com>
2024-02-05 09:06:39 -0300

    feat: :sparkles: Add validation

--- src/infrastructure/api/routes/transaction.route.ts
+++ src/infrastructure/api/routes/transaction.route.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance, FastifyPluginAsync } from 'fastify'
 import { TransactionController } from '../controllers/transaction.controller'
+import { transactionSchema } from '../../../modules/transaction/validation/transaction.schema'
 
 interface TransactionPayload {
   senderId: string
@@ -12,17 +13,22 @@ const transactionRoutes: FastifyPluginAsync = async (app: FastifyInstance) => {
     'TransactionController',
   )
 
-  app.post('/transaction', async (request, reply) => {
-    try {
-      const { senderId, receiverId, value } = request.body as TransactionPayload
-      const transactionPayload = { senderId, receiverId, value }
-      await transactionController.processTransaction(transactionPayload)
+  app.post(
+    '/transaction',
+    { schema: transactionSchema },
+    async (request, reply) => {
+      try {
+        const { senderId, receiverId, value } =
+          request.body as TransactionPayload
+        const transactionPayload = { senderId, receiverId, value }
+        await transactionController.processTransaction(transactionPayload)
 
-      reply.code(201).send({ success: true })
-    } catch (error: any) {
-      reply.code(500).send({ success: false, error })
-    }
-  })
+        reply.code(201).send({ success: true })
+      } catch (error: any) {
+        reply.code(500).send({ success: false, error })
+      }
+    },
+  )
 }
 
 export default transactionRoutes
